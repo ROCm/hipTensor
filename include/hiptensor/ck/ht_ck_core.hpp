b5d09e0ae3c717c2dd8cdc6e7c39ef4194b021da
Chaitanya Inumella <Chaitanya.Inumella@amd.com>
2022-07-12 21:12:26 +0000

    Remodularized the code with the new hiptensor APIs

--- include/hiptensor/ck/ht_ck_core.hpp
+++ include/hiptensor/ck/ht_ck_core.hpp
@@ -5,11 +5,28 @@
 
 hiptensorStatus_t hiptensorDeriveLayoutFromInputs(hiptensorContractionDescriptor_t* desc, const int ndim);
 
-hiptensorStatus_t hiptensorCKContraction(const hiptensorHandle_t* handle,
-                                     const hiptensorContractionPlan_t* plan,
-                                     hiptensorContractionMetrics_t *ht_contract_metrics,
-                                     const void* alpha, const void* A, const void* B,
-                                     const void* beta,  const void* C,       void* D,
-                                     void *workspace, uint64_t workspaceSize, hipStream_t stream);
+hiptensorStatus_t hiptensorCKScaleContraction(const hiptensorHandle_t* handle,
+                                    const hiptensorContractionPlan_t* plan,
+                                    const void* alpha, const void* A, const void* B,
+                                    const void* beta,  const void* C,       void* D,
+                                    void *workspace, uint64_t workspaceSize, hipStream_t stream);
 
+hiptensorStatus_t hiptensorCKBilinearContraction(const hiptensorHandle_t* handle,
+                                    const hiptensorContractionPlan_t* plan,
+                                    const void* alpha, const void* A, const void* B,
+                                    const void* beta,  const void* C,       void* D,
+                                    void *workspace, uint64_t workspaceSize, hipStream_t stream);
+/*
+hiptensorStatus_t hiptensorCKContraction(const hiptensorHandle_t* handle,
+                                    const hiptensorContractionPlan_t* plan,
+                                    hiptensorContractionMetrics_t *ht_contract_metrics,
+                                    const void* alpha, const void* A, const void* B,
+                                    const void* beta,  const void* C,       void* D,
+                                    void *workspace, uint64_t workspaceSize, hipStream_t stream);
+
+
+hiptensorStatus_t hiptensorInitCKContractionFind(const hiptensorHandle_t* handle,
+                                             hiptensorContractionFind_t* find,
+                                             const hiptensorAlgo_t algo);
+*/
 #endif
