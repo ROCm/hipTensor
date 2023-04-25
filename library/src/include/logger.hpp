c466a6d7acae9790cadb803f615191738521926b
Chris Millette <chris.millette@amd.com>
2023-04-25 13:43:33 -0400

    Add logging API

--- library/src/include/logger.hpp
+++ library/src/include/logger.hpp
@@ -31,54 +31,69 @@
 
 #include <mutex>
 
-namespace hiptensor {
-class Logger : public LazySingleton<Logger> {
-private:
-  using Callback_t = void (*)(int32_t logLevel, const char *funcName,
-                              const char *msg);
+namespace hiptensor
+{
+    class Logger : public LazySingleton<Logger>
+    {
+    private:
+        using Callback_t = void (*)(int32_t logLevel, const char* funcName, const char* msg);
 
-public:
-  enum Status_t : int32_t {
-    LOGGER_SUCCESS = 0,
-    LOGGER_BAD_FILE_NAME,
-    LOGGER_BAD_FILE_STREAM,
-    LOGGER_BAD_CALLBACK,
-    LOGGER_BAD_LOG_MASK,
-    LOGGER_FILE_OPEN_FAILED,
-  };
+    public:
+        enum struct Status_t : int32_t
+        {
+            SUCCESS = 0,
+            INVALID_FILE_NAME,
+            INVALID_FILE_STREAM,
+            INVALID_CALLBACK,
+            INVALID_LOG_MASK,
+            INVALID_LOG_LEVEL,
+            FILE_OPEN_FAILED,
+        };
 
-  // For static initialization
-  friend std::unique_ptr<Logger> std::make_unique<Logger>();
+        enum struct LogLevel_t : int32_t
+        {
+            LOG_LEVEL_OFF              = 0,
+            LOG_LEVEL_ERROR            = 1,
+            LOG_LEVEL_PERF_TRACE       = 2,
+            LOG_LEVEL_PERF_HINT        = 4,
+            LOG_LEVEL_HEURISTICS_TRACE = 8,
+            LOG_LEVEL_API_TRACE        = 16
+        };
 
-  Logger();
-  ~Logger();
+        // For static initialization
+        friend std::unique_ptr<Logger> std::make_unique<Logger>();
 
-  Status_t writeToStream(FILE *stream);
-  Status_t openFileStream(const char *fileName);
-  Status_t setCallback(Callback_t callbackFunc);
-  int32_t getLogMask() const;
-  Status_t setLogMask(int32_t mask);
-  void disable();
-  void enable();
+        ~Logger();
 
-  Status_t logMessage(int32_t context, const char *apiFuncName,
-                      const char *message);
+        Status_t writeToStream(FILE* stream);
+        Status_t openFileStream(const char* fileName);
+        Status_t setCallback(Callback_t callbackFunc);
+        int32_t  getLogMask() const;
+        Status_t setLogMask(int32_t mask);
+        Status_t setLogLevel(int32_t level);
+        void     disable();
+        void     enable();
 
-private:
-  static const char *timeStamp();
-  static int32_t appPid();
-  static const char *contextString(int32_t context);
+        Status_t logMessage(int32_t context, const char* apiFuncName, const char* message);
 
-private:
-  bool mEnabled;
-  bool mOwnsStream;
+        static const char* statusString(Status_t status);
 
-  int32_t mLogMask;
-  FILE *mWriteStream;
-  Callback_t mCallback;
+    private:
+        Logger();
+        static const char* timeStamp();
+        static int32_t     appPid();
+        static const char* contextString(LogLevel_t context);
 
-  mutable std::mutex mMutex;
-};
+    private:
+        bool mEnabled;
+        bool mOwnsStream;
+
+        int32_t    mLogMask;
+        FILE*      mWriteStream;
+        Callback_t mCallback;
+
+        mutable std::mutex mMutex;
+    };
 
 } // namespace hiptensor
 
--- library/src/logger.cpp
+++ library/src/logger.cpp
@@ -32,148 +32,236 @@
 
 #include <mutex>
 
-namespace hiptensor {
-Logger::Logger()
-    : mEnabled(true), mOwnsStream(false), mLogMask(0), mWriteStream(stdout),
-      mCallback(nullptr) {}
-
-Logger::~Logger() {
-  std::scoped_lock lock(mMutex);
-  if (mOwnsStream && mWriteStream != nullptr) {
-    fclose(mWriteStream);
-  }
-}
-
-Logger::Status_t Logger::writeToStream(FILE *stream) {
-  std::scoped_lock lock(mMutex);
-  if (stream != nullptr) {
-    if (mOwnsStream && mWriteStream != nullptr) {
-      fclose(mWriteStream);
-      mOwnsStream = false;
-    }
-    mWriteStream = stream;
-    return LOGGER_SUCCESS;
-  }
-
-  return LOGGER_BAD_FILE_STREAM;
-}
-
-Logger::Status_t Logger::openFileStream(const char *fileName) {
-  std::scoped_lock lock(mMutex);
-  if (fileName != nullptr && !strcmp(fileName, "")) {
-    if (mOwnsStream && mWriteStream != nullptr) {
-      fclose(mWriteStream);
-      mOwnsStream = false;
-    }
-    mWriteStream = fopen(fileName, "w");
-    if (mWriteStream == nullptr) {
-      return LOGGER_FILE_OPEN_FAILED;
-    }
-    mOwnsStream = true;
-    return LOGGER_SUCCESS;
-  }
-
-  // Revert back to stdout
-  mOwnsStream = false;
-  mWriteStream = stdout;
-  return LOGGER_BAD_FILE_NAME;
-}
-
-Logger::Status_t Logger::setCallback(Callback_t callbackFunc) {
-  std::scoped_lock lock(mMutex);
-  if (callbackFunc != nullptr) {
-    mCallback = callbackFunc;
-    return LOGGER_SUCCESS;
-  }
-  return LOGGER_BAD_CALLBACK;
-}
-
-int32_t Logger::getLogMask() const {
-  std::scoped_lock lock(mMutex);
-  return mLogMask;
-}
-
-Logger::Status_t Logger::setLogMask(int32_t mask) {
-  std::scoped_lock lock(mMutex);
-  if (mask >= 0) {
-    mLogMask = mask;
-    return LOGGER_SUCCESS;
-  }
-  return LOGGER_BAD_LOG_MASK;
-}
-
-void Logger::disable() {
-  std::scoped_lock lock(mMutex);
-  mEnabled = false;
-}
-
-void Logger::enable() {
-  std::scoped_lock lock(mMutex);
-  mEnabled = true;
-}
-
-Logger::Status_t Logger::logMessage(int32_t context, const char *apiFuncName,
-                                    const char *message) {
-  std::scoped_lock lock(mMutex);
-  if ((context & mLogMask) > 0 && mEnabled) {
-    char buff[2048];
-    sprintf(buff, "[%s][hipTensor][%d][%s][%s] %s", timeStamp(), appPid(),
-            contextString(context), apiFuncName, message);
-
-    if (mCallback != nullptr) {
-      (*mCallback)(context, apiFuncName, buff);
+namespace hiptensor
+{
+    Logger::Logger()
+        : mEnabled(true)
+        , mOwnsStream(false)
+        , mLogMask(0)
+        , mWriteStream(stdout)
+        , mCallback(nullptr)
+    {
     }
 
-    fprintf(mWriteStream, "%s", buff);
-  }
-  return LOGGER_SUCCESS;
-}
+    Logger::~Logger()
+    {
+        std::scoped_lock lock(mMutex);
+        if(mOwnsStream && mWriteStream != nullptr)
+        {
+            fclose(mWriteStream);
+        }
+    }
 
-/* static */
-const char *Logger::timeStamp() {
-  static std::mutex tsMutex;
-  std::scoped_lock lock(tsMutex);
+    Logger::Status_t Logger::writeToStream(FILE* stream)
+    {
+        std::scoped_lock lock(mMutex);
+        if(stream != nullptr)
+        {
+            if(mOwnsStream && mWriteStream != nullptr)
+            {
+                fclose(mWriteStream);
+                mOwnsStream = false;
+            }
+            mWriteStream = stream;
+            return Status_t::SUCCESS;
+        }
 
-  // Statically allocate buffer.
-  // Beware thread concurrency.
-  static char buff[32];
+        return Status_t::INVALID_FILE_STREAM;
+    }
 
-  time_t t;
-  struct tm *tInfo;
+    Logger::Status_t Logger::openFileStream(const char* fileName)
+    {
+        std::scoped_lock lock(mMutex);
+        if(fileName != nullptr && strcmp(fileName, "") != 0)
+        {
+            if(mOwnsStream && mWriteStream != nullptr)
+            {
+                fclose(mWriteStream);
+                mOwnsStream = false;
+            }
+            mWriteStream = fopen(fileName, "w");
+            if(mWriteStream == nullptr)
+            {
+                // Revert back to stdout
+                mOwnsStream  = false;
+                mWriteStream = stdout;
+                return Status_t::FILE_OPEN_FAILED;
+            }
+            mOwnsStream = true;
+            return Status_t::SUCCESS;
+        }
 
-  // Retrieve the time information
-  time(&t);
-  tInfo = localtime(&t);
+        // Revert back to stdout
+        mOwnsStream  = false;
+        mWriteStream = stdout;
+        return Status_t::INVALID_FILE_NAME;
+    }
 
-  // Format the timestamp string
-  // YYYY-MM-DD HH:MM:SS
-  strftime(buff, 32, "%F %T", tInfo);
-  return buff;
-}
+    Logger::Status_t Logger::setCallback(Callback_t callbackFunc)
+    {
+        std::scoped_lock lock(mMutex);
+        if(callbackFunc != nullptr)
+        {
+            mCallback = callbackFunc;
+            return Status_t::SUCCESS;
+        }
+        return Status_t::INVALID_CALLBACK;
+    }
 
-/* static */
-int32_t Logger::appPid() {
-  // App PID won't generally change
-  static int pid = getpid();
-  return pid;
-}
+    int32_t Logger::getLogMask() const
+    {
+        std::scoped_lock lock(mMutex);
+        return mLogMask;
+    }
 
-/* static */
-const char *Logger::contextString(int32_t context) {
-  // NOTE: MUST align with hiptensorLogLevel_t
-  switch (context) {
-  case 1:
-    return "Error";
-  case 2:
-    return "Performance";
-  case 4:
-    return "Kernel Selection";
-  case 8:
-    return "API";
-  default:
-    return "";
-  }
-  return "";
-}
+    Logger::Status_t Logger::setLogLevel(int32_t level)
+    {
+        std::scoped_lock lock(mMutex);
+        switch(level)
+        {
+        // Only accept discretized log level
+        case(int32_t)LogLevel_t::LOG_LEVEL_ERROR:
+        case(int32_t)LogLevel_t::LOG_LEVEL_PERF_TRACE:
+        case(int32_t)LogLevel_t::LOG_LEVEL_PERF_HINT:
+        case(int32_t)LogLevel_t::LOG_LEVEL_HEURISTICS_TRACE:
+        case(int32_t)LogLevel_t::LOG_LEVEL_API_TRACE:
+        {
+            mLogMask = level;
+            return Status_t::SUCCESS;
+        }
+        default:
+        {
+            return Status_t::INVALID_LOG_LEVEL;
+        }
+        }
+    }
+
+    Logger::Status_t Logger::setLogMask(int32_t mask)
+    {
+        std::scoped_lock lock(mMutex);
+        if(mask >= 0 && mask <= 0x1F)
+        {
+            mLogMask = mask;
+            return Status_t::SUCCESS;
+        }
+        return Status_t::INVALID_LOG_MASK;
+    }
+
+    void Logger::disable()
+    {
+        std::scoped_lock lock(mMutex);
+        mEnabled = false;
+    }
+
+    void Logger::enable()
+    {
+        std::scoped_lock lock(mMutex);
+        mEnabled = true;
+    }
+
+    Logger::Status_t
+        Logger::logMessage(int32_t context, const char* apiFuncName, const char* message)
+    {
+        std::scoped_lock lock(mMutex);
+        if((context & mLogMask) > 0 && mEnabled)
+        {
+            // Init message
+            char buff[2048];
+            sprintf(buff,
+                    "[%s][hipTensor][%d][%s][%s] %s\n",
+                    timeStamp(),
+                    appPid(),
+                    contextString((LogLevel_t)context),
+                    apiFuncName,
+                    message);
+
+            // Invoke logger callback
+            if(mCallback != nullptr)
+            {
+                (*mCallback)(context, apiFuncName, buff);
+            }
+
+            // Log to stream
+            fprintf(mWriteStream, "%s", buff);
+        }
+
+        return Status_t::SUCCESS;
+    }
+
+    /* static */
+    const char* Logger::timeStamp()
+    {
+        static std::mutex tsMutex;
+        std::scoped_lock  lock(tsMutex);
+
+        // Statically allocate buffer.
+        // Beware thread concurrency.
+        static char buff[32];
+
+        time_t     t;
+        struct tm* tInfo;
+
+        // Retrieve the time information
+        time(&t);
+        tInfo = localtime(&t);
+
+        // Format the timestamp string
+        // YYYY-MM-DD HH:MM:SS
+        strftime(buff, 32, "%F %T", tInfo);
+        return buff;
+    }
+
+    /* static */
+    const char* Logger::statusString(Status_t status)
+    {
+        switch(status)
+        {
+        case Status_t::SUCCESS:
+            return "SUCCESS";
+        case Status_t::INVALID_FILE_NAME:
+            return "INVALID_FILE_NAME";
+        case Status_t::INVALID_FILE_STREAM:
+            return "INVALID_FILE_STREAM";
+        case Status_t::INVALID_CALLBACK:
+            return "INVALID_CALLBACK";
+        case Status_t::INVALID_LOG_MASK:
+            return "INVALID_LOG_MASK";
+        case Status_t::INVALID_LOG_LEVEL:
+            return "INVALID_LOG_LEVEL";
+        case Status_t::FILE_OPEN_FAILED:
+            return "FILE_OPEN_FAILED";
+        default:
+            return "STATUS_UNKNOWN";
+        }
+    }
+
+    /* static */
+    int32_t Logger::appPid()
+    {
+        // App PID won't generally change
+        static int pid = getpid();
+        return pid;
+    }
+
+    /* static */
+    const char* Logger::contextString(LogLevel_t context)
+    {
+        // NOTE: MUST align with hiptensorLogLevel_t
+        switch(context)
+        {
+        case LogLevel_t::LOG_LEVEL_ERROR:
+            return "Error";
+        case LogLevel_t::LOG_LEVEL_PERF_TRACE:
+            return "Performance";
+        case LogLevel_t::LOG_LEVEL_PERF_HINT:
+            return "Performance Hint";
+        case LogLevel_t::LOG_LEVEL_HEURISTICS_TRACE:
+            return "Heuristics Trace";
+        case LogLevel_t::LOG_LEVEL_API_TRACE:
+            return "API";
+        default:
+            return "";
+        }
+    }
 
 } // namespace hiptensor
