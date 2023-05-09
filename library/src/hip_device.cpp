840295c207bc664b38457b5924a6d8b9a089a922
Meena Karunanidhi <85510608+mkarunan@users.noreply.github.com>
2023-05-09 16:40:58 -0400

    Add HIP Device Class setup  (#35)
    * Add hip device
    
    * Modify occurrences of HIP Error Checks
    
    * Add to CMake

---
+++ library/src/hip_device.cpp
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include "include/hip_device.hpp"
+
+namespace hiptensor
+{
+    HipDevice::HipDevice()
+        : mHandle(-1)
+        , mGcnArch(hipGcnArch_t::UNSUPPORTED_ARCH)
+        , mWarpSize(hipWarpSize_t::UNSUPPORTED_WARP_SIZE)
+        , mSharedMemSize(0)
+        , mCuCount(0)
+        , mMaxFreqMhz(0)
+    {
+        CHECK_HIP_ERROR(hipGetDevice(&mHandle));
+        CHECK_HIP_ERROR(hipGetDeviceProperties(&mProps, mHandle));
+
+        mArch = mProps.arch;
+
+        std::string deviceName(mProps.gcnArchName);
+
+        if(deviceName.find("gfx908") != std::string::npos)
+        {
+            mGcnArch = hipGcnArch_t::GFX908;
+        }
+        else if(deviceName.find("gfx90a") != std::string::npos)
+        {
+            mGcnArch = hipGcnArch_t::GFX90A;
+        }
+
+        switch(mProps.warpSize)
+        {
+        case hipWarpSize_t::Wave64:
+            mWarpSize = mProps.warpSize;
+        default:;
+        }
+
+        mSharedMemSize = mProps.sharedMemPerBlock;
+        mCuCount       = mProps.multiProcessorCount;
+        mMaxFreqMhz    = static_cast<int>(static_cast<double>(mProps.clockRate) / 1000.0);
+    }
+
+    hipDevice_t HipDevice::getDeviceHandle() const
+    {
+        return mHandle;
+    }
+
+    hipDeviceProp_t HipDevice::getDeviceProps() const
+    {
+        return mProps;
+    }
+
+    hipDeviceArch_t HipDevice::getDeviceArch() const
+    {
+        return mArch;
+    }
+
+    HipDevice::hipGcnArch_t HipDevice::getGcnArch() const
+    {
+        return mGcnArch;
+    }
+
+    int HipDevice::warpSize() const
+    {
+        return mWarpSize;
+    }
+
+    int HipDevice::sharedMemSize() const
+    {
+        return mSharedMemSize;
+    }
+
+    int HipDevice::cuCount() const
+    {
+        return mCuCount;
+    }
+
+    int HipDevice::maxFreqMhz() const
+    {
+        return mMaxFreqMhz;
+    }
+
+    bool HipDevice::supportsF64() const
+    {
+        return (mGcnArch == HipDevice::hipGcnArch_t::GFX90A);
+    }
+
+} // namespace hiptensor
