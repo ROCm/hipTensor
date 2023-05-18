09f59cb80ae71b024c3a5264f014feb886ae76ab
Meena Karunanidhi <Meena.Karunanidhi@amd.com>
2023-05-17 21:49:10 -0700

    Add gpu compare equal

--- test/01_contraction/common.hpp
+++ test/01_contraction/common.hpp
@@ -30,25 +30,38 @@
 #include <algorithm>
 #include <fstream>
 #include <iterator>
+#include <math.h>
+#include <mutex>
 #include <numeric>
 #include <unordered_map>
-#include <mutex>
 
 // hiptensor includes
 #include <hiptensor/hiptensor.hpp>
 #include <hiptensor/hiptensor_types.hpp>
 #include <hiptensor/internal/hiptensor_utility.hpp>
 
+#include "device/common.hpp"
+
 #define NDIM 4
+
+template <typename intT1,
+          class = typename std::enable_if<std::is_integral<intT1>::value>::type,
+          typename intT2,
+          class = typename std::enable_if<std::is_integral<intT2>::value>::type>
+static constexpr intT1 ceilDiv(const intT1 numerator, const intT2 divisor)
+{
+    return (numerator + divisor - 1) / divisor;
+}
 
 struct joinable_thread : std::thread
 {
     template <typename... Xs>
-    joinable_thread(Xs&&... xs) : std::thread(std::forward<Xs>(xs)...)
+    joinable_thread(Xs&&... xs)
+        : std::thread(std::forward<Xs>(xs)...)
     {
     }
 
-    joinable_thread(joinable_thread&&) = default;
+    joinable_thread(joinable_thread&&)            = default;
     joinable_thread& operator=(joinable_thread&&) = default;
 
     ~joinable_thread()
@@ -58,43 +71,41 @@ struct joinable_thread : std::thread
     }
 };
 
-template < typename ADataType,
-           typename BDataType,
-           typename DDataType,
-           typename floatTypeCompute>
-void hiptensorScaleContractionReference(ADataType *A,
-                                        BDataType *B,
-                                        DDataType *D,
-                                        floatTypeCompute alpha,
+template <typename ADataType, typename BDataType, typename DDataType, typename floatTypeCompute>
+void hiptensorScaleContractionReference(ADataType*           A,
+                                        BDataType*           B,
+                                        DDataType*           D,
+                                        floatTypeCompute     alpha,
                                         std::vector<int64_t> a_ms_ks_lengths,
                                         std::vector<int64_t> b_ks_ns_lengths,
                                         std::vector<int64_t> d_ms_ns_lengths,
-                                        std::vector<size_t> a_ms_ks_strides,
-                                        std::vector<size_t> b_ks_ns_strides,
-                                        std::vector<size_t> d_ms_ns_strides,
-                                        std::size_t elementsD,
-                                        std::size_t num_thread = 1)
+                                        std::vector<size_t>  a_ms_ks_strides,
+                                        std::vector<size_t>  b_ks_ns_strides,
+                                        std::vector<size_t>  d_ms_ns_strides,
+                                        std::size_t          elementsD,
+                                        std::size_t          num_thread = 1)
 {
-    auto d_ms_ns = [&](auto m0, auto m1, auto n0, auto n1)
-    {
+    auto d_ms_ns = [&](auto m0, auto m1, auto n0, auto n1) {
         floatTypeCompute valA, valB, valC, valAcc = 0;
-        size_t indexA, indexB, indexD;
+        size_t           indexA, indexB, indexD;
 
         auto K0 = a_ms_ks_lengths[2];
         auto K1 = a_ms_ks_lengths[3];
 
         auto offset = [&](std::vector<size_t> curIndices, std::vector<size_t> strides) {
-                        return std::inner_product(curIndices.begin(), curIndices.end(), strides.begin(), std::size_t{0}); };
+            return std::inner_product(
+                curIndices.begin(), curIndices.end(), strides.begin(), std::size_t{0});
+        };
 
         for(size_t k0 = 0; k0 < K0; k0++)
         {
             for(size_t k1 = 0; k1 < K1; k1++)
             {
                 indexA = offset(std::vector<size_t>{m0, m1, k0, k1}, a_ms_ks_strides);
-                valA = static_cast<floatTypeCompute> (A[indexA]);
+                valA   = static_cast<floatTypeCompute>(A[indexA]);
 
                 indexB = offset(std::vector<size_t>{n0, n1, k0, k1}, b_ks_ns_strides);
-                valB = static_cast<floatTypeCompute> (B[indexB]);
+                valB   = static_cast<floatTypeCompute>(B[indexB]);
 
                 valAcc += valA * valB;
             }
@@ -102,14 +113,14 @@ void hiptensorScaleContractionReference(ADataType *A,
 
         valC = alpha * valAcc;
 
-        indexD = offset(std::vector<size_t>{m0, m1, n0, n1}, d_ms_ns_strides);
+        indexD    = offset(std::vector<size_t>{m0, m1, n0, n1}, d_ms_ns_strides);
         D[indexD] = static_cast<DDataType>(valC);
     };
 
     auto GetNdIndices = [&](size_t index) {
         std::array<std::size_t, NDIM> indices;
 
-        for(std::size_t idim = 0;  idim < NDIM; ++idim)
+        for(std::size_t idim = 0; idim < NDIM; ++idim)
         {
             indices[idim] = index / d_ms_ns_strides[idim];
             index -= indices[idim] * d_ms_ns_strides[idim];
@@ -118,7 +129,7 @@ void hiptensorScaleContractionReference(ADataType *A,
         return indices;
     };
 
-    std::size_t work_per_thread = (elementsD + num_thread - 1) / num_thread;
+    std::size_t                  work_per_thread = (elementsD + num_thread - 1) / num_thread;
     std::vector<joinable_thread> threads(num_thread);
 
     for(std::size_t i = 0; i < num_thread; ++i)
@@ -138,49 +149,49 @@ void hiptensorScaleContractionReference(ADataType *A,
     }
 }
 
-template < typename ADataType,
-           typename BDataType,
-           typename CDataType,
-           typename DDataType,
-           typename floatTypeCompute>
-void hiptensorBilinearContractionReference(ADataType *A,
-                                           BDataType *B,
-                                           CDataType *C,
-                                           DDataType *D,
-                                           floatTypeCompute alpha,
-                                           floatTypeCompute beta,
+template <typename ADataType,
+          typename BDataType,
+          typename CDataType,
+          typename DDataType,
+          typename floatTypeCompute>
+void hiptensorBilinearContractionReference(ADataType*           A,
+                                           BDataType*           B,
+                                           CDataType*           C,
+                                           DDataType*           D,
+                                           floatTypeCompute     alpha,
+                                           floatTypeCompute     beta,
                                            std::vector<int64_t> a_ms_ks_lengths,
                                            std::vector<int64_t> b_ks_ns_lengths,
                                            std::vector<int64_t> c_ms_ns_lengths,
                                            std::vector<int64_t> d_ms_ns_lengths,
-                                           std::vector<size_t> a_ms_ks_strides,
-                                           std::vector<size_t> b_ks_ns_strides,
-                                           std::vector<size_t> c_ms_ns_strides,
-                                           std::vector<size_t> d_ms_ns_strides,
-                                           std::size_t elementsD,
-                                           std::size_t num_thread = 1)
+                                           std::vector<size_t>  a_ms_ks_strides,
+                                           std::vector<size_t>  b_ks_ns_strides,
+                                           std::vector<size_t>  c_ms_ns_strides,
+                                           std::vector<size_t>  d_ms_ns_strides,
+                                           std::size_t          elementsD,
+                                           std::size_t          num_thread = 1)
 {
-    auto d_ms_ns = [&](auto m0, auto m1, auto n0, auto n1)
-    {
+    auto d_ms_ns = [&](auto m0, auto m1, auto n0, auto n1) {
         floatTypeCompute valA, valB, valAcc = 0, valD1, valD2;
-        size_t indexA, indexB, indexC, indexD;
+        size_t           indexA, indexB, indexC, indexD;
 
         auto K0 = a_ms_ks_lengths[2];
         auto K1 = a_ms_ks_lengths[3];
 
         auto offset = [&](std::vector<size_t> curIndices, std::vector<size_t> strides) {
-                        return std::inner_product(curIndices.begin(), curIndices.end(),
-                                                  strides.begin(), std::size_t{0}); };
+            return std::inner_product(
+                curIndices.begin(), curIndices.end(), strides.begin(), std::size_t{0});
+        };
 
         for(size_t k0 = 0; k0 < K0; k0++)
         {
             for(size_t k1 = 0; k1 < K1; k1++)
             {
                 indexA = offset(std::vector<size_t>{m0, m1, k0, k1}, a_ms_ks_strides);
-                valA = static_cast<floatTypeCompute> (A[indexA]);
+                valA   = static_cast<floatTypeCompute>(A[indexA]);
 
                 indexB = offset(std::vector<size_t>{n0, n1, k0, k1}, b_ks_ns_strides);
-                valB = static_cast<floatTypeCompute> (B[indexB]);
+                valB   = static_cast<floatTypeCompute>(B[indexB]);
 
                 valAcc += valA * valB;
             }
@@ -189,16 +200,16 @@ void hiptensorBilinearContractionReference(ADataType *A,
         valD1 = valAcc * alpha;
 
         indexC = offset(std::vector<size_t>{m0, m1, n0, n1}, c_ms_ns_strides);
-        valD2 = static_cast<floatTypeCompute>(C[indexC]) * beta;
+        valD2  = static_cast<floatTypeCompute>(C[indexC]) * beta;
 
-        indexD = offset(std::vector<size_t>{m0, m1, n0, n1}, d_ms_ns_strides);
+        indexD    = offset(std::vector<size_t>{m0, m1, n0, n1}, d_ms_ns_strides);
         D[indexD] = static_cast<DDataType>(valD1 + valD2);
     };
 
     auto GetNdIndices = [&](size_t index) {
         std::array<std::size_t, NDIM> indices;
 
-        for(std::size_t idim = 0;  idim < NDIM; ++idim)
+        for(std::size_t idim = 0; idim < NDIM; ++idim)
         {
             indices[idim] = index / d_ms_ns_strides[idim];
             index -= indices[idim] * d_ms_ns_strides[idim];
@@ -207,7 +218,7 @@ void hiptensorBilinearContractionReference(ADataType *A,
         return indices;
     };
 
-    std::size_t work_per_thread = (elementsD + num_thread - 1) / num_thread;
+    std::size_t                  work_per_thread = (elementsD + num_thread - 1) / num_thread;
     std::vector<joinable_thread> threads(num_thread);
 
     for(std::size_t i = 0; i < num_thread; ++i)
@@ -230,8 +241,8 @@ void hiptensorBilinearContractionReference(ADataType *A,
 template <typename DDataType>
 std::pair<bool, double> compareEqual(DDataType const* deviceD,
                                      DDataType const* hostD,
-                                     std::size_t elementsD,
-                                     double       tolerance = 100.0)
+                                     std::size_t      elementsD,
+                                     double           tolerance = 100.0)
 {
     bool   retval             = true;
     double max_relative_error = 0.0;
@@ -247,7 +258,7 @@ std::pair<bool, double> compareEqual(DDataType const* deviceD,
     for(int i = 0; i < elementsD; ++i)
     {
         auto valDevice = deviceD[i];
-        auto valHost = hostD[i];
+        auto valHost   = hostD[i];
 
         auto numerator = fabs(toDouble(valDevice) - toDouble(valHost));
         auto divisor   = fabs(toDouble(valDevice)) + fabs(toDouble(valHost)) + 1.0;
@@ -299,6 +310,86 @@ std::pair<bool, double> compareEqual(DDataType const* deviceD,
     }
 
     return std::make_pair(retval, max_relative_error);
+}
+
+template <typename DDataType>
+std::pair<bool, double> compareEqualLaunchKernel(DDataType*  deviceD,
+                                                 DDataType*  hostD,
+                                                 std::size_t elementsD,
+                                                 double      tolerance = 100.0)
+{
+    auto blockDim = dim3(1024, 1, 1);
+    auto gridDim  = dim3(ceilDiv(elementsD, blockDim.x), 1, 1);
+
+    double* d_relativeError;
+    double  maxRelativeError;
+    CHECK_HIP_ERROR(hipMalloc(&d_relativeError, elementsD * sizeof(double)));
+
+    hipEvent_t syncEvent;
+    CHECK_HIP_ERROR(hipEventCreate(&syncEvent));
+
+    // Calculate the relative error for each element of Tensor D
+    hipLaunchKernelGGL((compareEqualKernel<DDataType>),
+                       gridDim,
+                       blockDim,
+                       0,
+                       0,
+                       deviceD,
+                       hostD,
+                       d_relativeError,
+                       elementsD);
+    CHECK_HIP_ERROR(hipEventRecord(syncEvent));
+    CHECK_HIP_ERROR(hipEventSynchronize(syncEvent));
+
+    // Determine the maximum relative error
+    blockDim             = dim3(512, 1, 1);
+    uint32_t maxElements = 1024;
+    uint32_t offset      = 1;
+
+    for(uint32_t i = elementsD; i > 1; i = ceilDiv(i, maxElements))
+    {
+        gridDim       = dim3(ceilDiv(i, maxElements), 1, 1);
+        auto elements = i > maxElements ? maxElements : i;
+
+        hipLaunchKernelGGL((maxReduceKernel),
+                           gridDim,
+                           blockDim,
+                           0,
+                           0,
+                           d_relativeError,
+                           elements,
+                           offset,
+                           elementsD);
+
+        CHECK_HIP_ERROR(hipEventRecord(syncEvent));
+        CHECK_HIP_ERROR(hipEventSynchronize(syncEvent));
+        offset = offset * maxElements;
+    }
+
+    CHECK_HIP_ERROR(
+        hipMemcpy(&maxRelativeError, d_relativeError, sizeof(double), hipMemcpyDeviceToHost));
+
+    // Free allocated device memory
+    CHECK_HIP_ERROR(hipFree(d_relativeError));
+
+    bool retval = true;
+    bool isNaN  = std::isnan(maxRelativeError);
+
+    auto toDouble
+        = [](DDataType const& val) { return static_cast<double>(static_cast<float>(val)); };
+
+    auto eps = toDouble(std::numeric_limits<DDataType>::epsilon());
+    if(isNaN)
+    {
+        retval           = false;
+        maxRelativeError = std::numeric_limits<DDataType>::signaling_NaN();
+    }
+    else if(maxRelativeError > (eps * tolerance))
+    {
+        retval = false;
+    }
+
+    return std::make_pair(retval, maxRelativeError);
 }
 
 #endif // HIPTENSOR_TEST_CONTRACTION_COMMON_HPP
