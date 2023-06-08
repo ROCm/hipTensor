3bae73c7ac539dee92ec1c8e7d1cf32be9b88780
Meena Karunanidhi <85510608+mkarunan@users.noreply.github.com>
2023-06-08 13:43:24 -0400

    Update Version (#70)
    [ROCm/hipTensor commit: e7828ee91239486145f9803b554b27e0b1e24284]

--- test/00_unit/logger/logger_test.cpp
+++ test/00_unit/logger/logger_test.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright 2023 Advanced Micro Devices, Inc.
+ * Copyright (c) 2023 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,8 +19,8 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  *******************************************************************************/
 #include <iostream>
