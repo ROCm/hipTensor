dd609e5dc1a19093ded7e060070453eef7a9dfa1
Christopher Millette <63608002+cgmillette@users.noreply.github.com>
2023-09-05 16:43:23 -0600

    Merge new test-infrastructure into develop (#99)
    * Added resource classes & tested in bilinear_ac_f32 (#90)
    
    * Added resource classes & tested in bilinear_ac_f32
    
    * Add initial parameters for hipTensor tests (#85)
    
    * Add initial parameters for hipTensor tests
    
    * Add kernel
    
    * Add NONE_TYPE
    
    * Update Cmake
    
    * Initial yaml-config (#93)
    
    * Initial yaml-config
    
    * Add llvm component to for yaml parser and setup command line opts
    
    * Fixup include dirs
    
    * Command line arguments and GTest integration (#96)
    
    * initial commit
    
    * temp commit
    
    * fixed initial gtest integration
    
    * reworked options & test params
    
    * Added testParams to options class
    
    * cmd line & default yaml file parsing
    
    * cleanup & differentiated yaml files
    
    * Updated GTest git tag & yaml_test
    
    * Test infra (#97)
    
    * Add kernel setup
    
    * Add function ceildiv
    
    * Remove implementation for contraction test
    
    * Remove warning
    
    * Fix scale params
    
    * Update bilinear_contraction_f32_test.cpp
    
    * Add check device setup
    
    * Rename scale yaml files to match with cpp
    
    * Refactor
    
    * Remove extranneous tests
    
    * Test file cleanup
    
    * Bundle yaml configs to contraction test binaries
    
    * Adjust contraction tests to bundle YAML configs
    
    * Reduce some verbosity
    
    * Turn off clang formatting for YAML template file
    
    ---------
    
    Co-authored-by: David Langbehn <47395968+dlangbe@users.noreply.github.com>
    Co-authored-by: Meena Karunanidhi <85510608+mkarunan@users.noreply.github.com>

---
+++ test/01_contraction/contraction_resource.hpp
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2021-2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#ifndef HIPTENSOR_CONTRACTION_RESOURCE_HPP
+#define HIPTENSOR_CONTRACTION_RESOURCE_HPP
+
+#include <memory>
+#include <tuple>
+
+#include "../hip_resource.hpp"
+#include "../singleton.hpp"
+
+// ContractionResource class is intended to manage a shared pool of resources for
+// testing hiptensor contraction kernels on the GPU.
+//
+// It minimizes the memory handling overhead for launching thousands of GPU
+// kernels by allowing re-use of existing memory allocations. Memory is only
+// re-allocated as necessary to satisfy minimum size requirements.
+//
+// The interface indicates memory ownership by this class and shall only be
+// used to access for read/write purposes.
+//
+// Currently uses HIP as the backend for device allocation.
+
+namespace hiptensor
+{
+
+    struct ContractionResource : public HipResource, public LazySingleton<ContractionResource>
+    {
+        // For static initialization
+        friend std::unique_ptr<ContractionResource>
+            std::make_unique<ContractionResource>();
+
+        using Base = HipResource;
+
+    public:
+        using DevicePtrT = Base::DevicePtrT;
+        using HostPtrT = Base::HostPtrT;
+
+        // M, N, U, V, H, K
+        using ProblemDims = std::vector<std::size_t>;;
+
+        // MatrixA, MatrixB, MatrixC, MatrixD (# of elements)
+        using MatrixElements = std::tuple<int64_t, int64_t, int64_t, int64_t>;
+
+        // Bytes per element for matrices A/B/C/D
+        using ElementBytes = std::tuple<int32_t, int32_t, int32_t, int32_t>;
+
+        enum : uint32_t
+        {
+            // Matrix size indices
+            MatrixA = 0,
+            MatrixB = 1,
+            MatrixC = 2,
+            MatrixD = 3,
+
+            // Problem size indices
+            M = 0,
+            N = 1,
+            U = 2,
+            V = 3,
+            H = 4,
+            K = 5
+        };
+
+    private: // No public instantiation except make_unique.
+             // No copy
+        ContractionResource();
+        ContractionResource(const ContractionResource&)            = delete;
+        ContractionResource& operator=(const ContractionResource&) = delete;
+
+    public:
+        ContractionResource(ContractionResource&&);
+        ~ContractionResource() = default;
+
+        void copyHostToDeviceAll(ElementBytes const& bytesPerElement);
+        void copyDeviceToHostAll(ElementBytes const& bytesPerElement);
+        void resizeStorage(ProblemDims const& size, ElementBytes bytesPerElement);
+        void resizeStorage(MatrixElements const& size, ElementBytes bytesPerElement);
+
+        HostPtrT& hostA();
+        HostPtrT& hostB();
+        HostPtrT& hostC();
+        HostPtrT& hostD();
+
+        DevicePtrT& deviceA();
+        DevicePtrT& deviceB();
+        DevicePtrT& deviceC();
+        DevicePtrT& deviceD();
+
+        void reset() final;
+
+    protected:
+        DevicePtrT  mDeviceA, mDeviceB;
+        DevicePtrT mDeviceC, mDeviceD;
+        HostPtrT    mHostA, mHostB;
+        HostPtrT   mHostC, mHostD;
+        MatrixElements      mCurrentMatrixElements;
+        MatrixElements      mCurrentAllocElements;
+    };
+
+} // namespace hiptensor
+
+#endif // HIPTENSOR_CONTRACTION_RESOURCE_HPP
---
+++ test/hip_resource.cpp
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2021-2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#ifndef HIPTENSOR_HIP_RESOURCE_IMPL_HPP
+#define HIPTENSOR_HIP_RESOURCE_IMPL_HPP
+
+#include <hip/hip_runtime_api.h>
+
+#include "hip_resource.hpp"
+#include <hiptensor/internal/hiptensor_utility.hpp>
+
+namespace hiptensor
+{
+
+    auto HipResource::allocDevice(int64_t numBytes) -> DevicePtrT
+    {
+        char* data;
+        CHECK_HIP_ERROR(hipMalloc(&data, numBytes));
+        return DevicePtrT(data, DeviceDeleter());
+    }
+
+    void HipResource::reallocDevice(DevicePtrT& devicePtr, int64_t numBytes)
+    {
+        // Free existing ptr first before alloc in case of big sizes.
+        devicePtr.reset(nullptr);
+        devicePtr = std::move(allocDevice(numBytes));
+    }
+
+    auto HipResource::allocHost(int64_t numBytes) -> HostPtrT
+    {
+        void*  data;
+        data = operator new(numBytes);
+        return HostPtrT(data, HostDeleter());
+    }
+
+    void HipResource::reallocHost(HostPtrT& hostPtr, int64_t numBytes)
+    {
+        // Free existing ptr first before alloc in case of big sizes.
+        hostPtr.reset(nullptr);
+        hostPtr = std::move(allocHost(numBytes));
+    }
+
+    void HipResource::reallocDeviceHostPair(DevicePtrT& devicePtr,
+                                            HostPtrT&   hostPtr,
+                                            int64_t     numBytes)
+    {
+        reallocDevice(devicePtr, numBytes);
+        reallocHost(hostPtr, numBytes);
+    }
+
+    void HipResource::copyData(HostPtrT& dst, DevicePtrT const& src, int64_t numBytes)
+    {
+        CHECK_HIP_ERROR(hipMemcpy(dst.get(), src.get(), numBytes, hipMemcpyDeviceToHost));
+    }
+
+    void HipResource::copyData(DevicePtrT& dst, HostPtrT const& src, int64_t numBytes)
+    {
+        CHECK_HIP_ERROR(hipMemcpy(dst.get(), src.get(), numBytes, hipMemcpyHostToDevice));
+    }
+
+    void HipResource::copyData(HostPtrT& dst, HostPtrT const& src, int64_t numBytes)
+    {
+        CHECK_HIP_ERROR(hipMemcpy(dst.get(), src.get(), numBytes, hipMemcpyHostToHost));
+    }
+
+    void HipResource::copyData(DevicePtrT& dst, DevicePtrT const& src, int64_t numBytes)
+    {
+        CHECK_HIP_ERROR(hipMemcpy(dst.get(), src.get(), numBytes, hipMemcpyDeviceToDevice));
+    }
+
+} // namespace hiptensor
+
+#endif //HIPTENSOR_HIP_RESOURCE_IMPL_HPP
